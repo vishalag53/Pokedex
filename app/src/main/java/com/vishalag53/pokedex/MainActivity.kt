f3cfd5f1c8957fe74ed9d07c8806209462dc0e41
vishalag53 <vishal.agrawal1207@gmail.com>
2023-06-27 21:48:37 +0530

    Add Splash Screen and Icon

---
+++ app/src/main/java/com/vishalag53/pokedex/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.vishalag53.pokedex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.vishalag53.pokedex.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+    }
+}
